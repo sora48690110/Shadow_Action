565496c8ed448085b7781ec350cea4c49be94798
sora48690110 <misaki48690110@gmail.com>
2021-03-01 04:38:48 +0900

    ステージの反転作成

--- Shadow_Action/Assets/Scripts/Stage/StageDuplicate.cs
+++ Shadow_Action/Assets/Scripts/Stage/StageDuplicate.cs
@@ -8,35 +8,44 @@ public class StageDuplicate : MonoBehaviour
     //参考サイト
     //https://qiita.com/dynamonda/items/2368c24edef187775bcb
 
-
-
     [SerializeField] GameObject assetComponentOrGameObject;
     GameObject Sample;
     string assetPath = "Assets/Prefab/StageParent.prefab";
-    string assetPath2 = "Assets/Prefab/Stage/StageParent.prefab";
+    string Start_Path = "Assets/Prefab/Stage/StageParent.prefab";
     void Start()
     {
+        DontDestroyOnLoad(gameObject);
         //プレハブをそのままの位置で生成
         //PrefabUtility.InstantiatePrefab(assetComponentOrGameObject);
+        switch (gameObject.GetComponent<StageMovement>().NowStage)
+        {
+            case "SampleScene":
+                //指定パスの場所にプレハブを更新（パスに.prefab、まで指定すること）
+                Sample = PrefabUtility.LoadPrefabContents(Start_Path);
+                //オブジェクト名から（clone）を消す
+                GameObject obj = Instantiate(Sample);
+                obj.name = Sample.name;
+                //*********************************
+                
+                break;
+            case "SampleScene2":
+                //指定パスの場所にプレハブを更新（パスに.prefab、まで指定すること）
+                Sample = PrefabUtility.LoadPrefabContents(assetPath);
+                //オブジェクト名から（clone）を消す&上下反転
+                obj = Instantiate(Sample);
+                obj.transform.rotation = Quaternion.AngleAxis(180, new Vector3(1, 0, 0));
+                break;
+            default:
+                break;
 
+        }
+        PrefabUtility.SaveAsPrefabAssetAndConnect(Sample, assetPath, InteractionMode.AutomatedAction);
+        PrefabUtility.UnloadPrefabContents(Sample);
 
-
-        //指定パスの場所にプレハブを更新（パスに.prefab、まで指定すること）
-         Sample = PrefabUtility.LoadPrefabContents(assetPath2);
-        //オブジェクト名から（clone）を消す&上下反転
-        var obj= Instantiate(Sample)as GameObject;
-        obj.transform.localScale = new Vector3(1, -1, 1);
-        obj.name = Sample.name;
-        //*********************************
 
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            PrefabUtility.SaveAsPrefabAssetAndConnect(Sample, assetPath, InteractionMode.AutomatedAction);
-            PrefabUtility.UnloadPrefabContents(Sample);
-        }
     }
 }
---
+++ Shadow_Action/Assets/Scripts/Stage/StageMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class StageMovement : MonoBehaviour
+{
+    public string NowStage { get; set; }
+
+    void Awake()
+    {
+        NowStage = SceneManager.GetActiveScene().name;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Change_Stage();
+        }
+    }
+
+    //ステージ切り替え
+    void Change_Stage()
+    {
+        NowStage = SceneManager.GetActiveScene().name;
+        switch (NowStage)
+        {
+            case "SampleScene":
+                SceneManager.LoadScene("SampleScene2");
+                break;
+            case "SampleScene2":
+                SceneManager.LoadScene("SampleScene");
+                break;
+            default:
+                break;
+        }
+        //ステージ反転作成のために無駄に見えるが必要
+        //ステージ切り替えの際にGameDirectorが重複しないように削除
+        SceneManager.MoveGameObjectToScene(gameObject, SceneManager.GetActiveScene());
+        //********************************************************
+    }
+    //******************
+}
