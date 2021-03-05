dfd3bccefe21c2ce2fb8cbbc3c1bc170bab7146d
sora48690110 <misaki48690110@gmail.com>
2021-03-05 09:50:18 +0900

    位置同期等できた

--- Shadow_Action/Assets/Scripts/Stage/StageDuplicate.cs
+++ Shadow_Action/Assets/Scripts/Stage/StageDuplicate.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.Linq;
 
 public class StageDuplicate : MonoBehaviour
 {
     //参考サイト
     //https://qiita.com/dynamonda/items/2368c24edef187775bcb
+    //https://www.hanachiru-blog.com/entry/2019/07/20/000000
     GameObject obj;
-    [SerializeField] GameObject assetComponentOrGameObject;
+    //[SerializeField] GameObject assetComponentOrGameObject;
     GameObject Sample;
+
+
     string assetPath = "Assets/Resouces/Stage/NextParent.prefab";
     string Start_Path = "Assets/Resouces/Stage/StageParent.prefab";
     void Start()
@@ -49,10 +53,16 @@ public class StageDuplicate : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
+
+            GameObject[] childGameObjects = obj.GetComponentsInChildren<Transform>().Select(t => t.gameObject).ToArray();
             Sample = PrefabUtility.LoadPrefabContents(assetPath);
-            Sample.transform.position = obj.transform.position;
-            Sample.transform.rotation = obj.transform.rotation;
-            Sample.transform.localScale = obj.transform.localScale;
+            GameObject[] ParentGameObjects = Sample.GetComponentsInChildren<Transform>().Select(t => t.gameObject).ToArray();
+            for (int i = 0; i < childGameObjects.Length; i++)
+            {
+                ParentGameObjects[i].transform.position = childGameObjects[i].transform.position;
+                ParentGameObjects[i].transform.rotation = childGameObjects[i].transform.rotation;
+                ParentGameObjects[i].transform.localScale = childGameObjects[i].transform.localScale;
+            }
             //プレハブの中身更新
             PrefabUtility.SaveAsPrefabAssetAndConnect(Sample, assetPath,InteractionMode.AutomatedAction);
             PrefabUtility.UnloadPrefabContents(Sample);
